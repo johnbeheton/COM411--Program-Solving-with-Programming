c84960dde0368850d7998d2b415cffd6f6bef7fd
repl.it user <replituser@example.com>
2021-02-26 11:12:46 +0000

    Aded code lines

---
+++ main.py
@@ -0,0 +1,7 @@
+#Display message to standart output in multipleline of code
+print("SystemFailure Imminent")
+print("System Reboot starting...\n")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine\n")
+print("System online")
