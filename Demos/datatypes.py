846b554bb727d40ed09937f25020fb603f26313b
repl.it user <replituser@example.com>
2021-03-02 10:04:56 +0000

    Week 1 demos

---
+++ Demos/datatypes.py
@@ -0,0 +1,9 @@
+print("What is your name")
+#Variable is a container, which can store data for us in memory(string, intiger, float, bool)
+name = input()
+print("What is your age?")
+age = int(input())
+print("What is your bank balance?")
+balance = float(input())
+
+print("Welcome {}. Your are said to be {} years old. Your bank balance is {:.2f}.".format(name, age, balance))
---
+++ Demos/gitcycle.py
@@ -0,0 +1,5 @@
+#Display message to standart output in multipleline of code
+print("anything I want")
+print("1. git add .\n")
+print("2. git commit -m \"your message\"")
+print("3. git push")
