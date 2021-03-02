a72ecbd422c352612a9d086a31d0ada9c57b159b
repl.it user <replituser@example.com>
2021-03-02 00:10:17 +0000

    Test Ascii

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,7 @@
+#Displays Escape Characters
+
+print("\n = Displays new line")
+print("\t = Displays tab space")
+print("\\ = Displays backslash")
+print("\ = Displays double quote")
+print("\' = Displays single quote")
