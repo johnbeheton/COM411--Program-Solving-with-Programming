3abe326fbaf978a2c00d2d384c792fe1b2d1794e
repl.it user <replituser@example.com>
2021-03-16 11:34:17 +0000

    Fruits

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-print("What is your name?")
-n = input()
-print("Do you have a dog? (type True or False)")
-dog = input()
-#bool is boolean - store True/False
+def get_fruits():
+  fruits = []
+  print("How many fruits would you enter?")
+  n = int(input())
 
-if len(n) > 9 and dog == "True":
- print("You have a very long name!")
- print("Your name contains {} letters".format(len(n)))
-else:
-  print("Your name is quite ok")
-print("This is the END of the program!")
+  for i in range(4):
+    print("Type in the next fruit:")
+    fruits.append(input())
+
+  
+  #Print All
+  print("Your fruits are{}".format(fruits))
+
+  #Print few
+  print(f"Sliced list: {fruits[0:5:2]}")
+
+  #Print only few using negative Index
+  print(f"Negative index: {fruits[-2]}")
+
+
+get_fruits()    
