3def9d3acfbaaba9bb4d3edf7e86bfef94a30c57
repl.it user <replituser@example.com>
2021-03-19 16:58:03 +0000

    list

--- Demos/datatypes.py
+++ Demos/datatypes.py
@@ -7,3 +7,20 @@ print("What is your bank balance?")
 balance = float(input())
 
 print("Welcome {}. Your are said to be {} years old. Your bank balance is £{:.2f}.".format(name, age, balance))
+
+print("What is your name?")
+n = input()
+#print("Do you have a dog? (type True or False)")
+#dog = input()
+#bool is boolean - datatype store True/False
+
+if len(n) >= 9: #allow 9 and greater
+ print("You have a very long name!")
+ print("Your name contains {} letters".format(len(n)))
+elif len(n) > 6:
+  print("Your name is a bit long Consider a nickname")
+elif len(n) < 3:
+  print("Your name is very short")
+else:
+  print("Your name is quite ok")
+print("This is the END of the program!")
