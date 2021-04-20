13eb0608a2b37ad0762724ba4347b31715fef76f
johnbeheton <>
2021-04-20 09:49:17 +0000

    Practice

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-file = open("gaga.txt", "w")
-for i in file.readlines():
-   print(file.read())
-file.close()
+f1 = open("john.txt")
+f2 = open("harry,txt")
+
+for i in range (3):
+  print(f"\033[92mjohn: {f1.readline()}\033[0m anything" , end= "")
+  print(f"\033[92mharry: {f1.readline()}\033[0m anything" , end= "")
+
+  f1, f2.close()
